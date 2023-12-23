12bb06c44528f6fa15d22c632874c0cfa9b8181e
mkaraskova <martinakarask@gmail.com>
2023-12-23 20:17:35 +0100

    dashboard progress, footer fixed

--- eMood_app/static/dashboard.js
+++ eMood_app/static/dashboard.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
-    $('.dropdown-toggle').click(function () {
-        $(this).next('.dropdown-menu').slideToggle();
-    });
-
+    displayUserData();
     $('#addPersonForm').on('submit', function (e) {
         e.preventDefault();
 
@@ -28,39 +25,81 @@ $(document).ready(function () {
             })
             .catch(error => console.log('error', error));
     });
-
-    function fetchUserInfo(userName) {
-        $.ajax({
-            url: `/dashboard/${userName}`,
-            type: 'GET',
-            success: function (data) {
-                // Update the content with user information
-                displayUserData(data);
-            },
-            error: function (error) {
-                console.error('Error fetching user info:', error);
-            }
-        });
-    }
-
-    // Call fetchUserInfo when a user is clicked in the dropdown
-    $('.dropdown-item').click(function (e) {
-        e.preventDefault();
-        var userName = $(this).text().trim();
-
-        // Update URL without reloading the page
-        history.pushState({}, '', `/dashboard/${userName}`);
-
-        fetchUserInfo(userName);
-    });
 });
 
-function updateDropdownText(selectedOption) {
-    document.getElementById('dropdownMenuButton').innerText = selectedOption + '\u00A0\u00A0\u00A0';
-    var dropdownToggle = document.querySelector('.dropdown-toggle');
-    dropdownToggle.click();
+function fetchUserInfo(userName) {
+    $.ajax({
+        url: `/dashboard/${userName}`,
+        type: 'GET',
+        success: function (data) {
+            // Update the content with user information
+            displayUserData(data);
+        },
+        error: function (error) {
+            console.error('Error fetching user info:', error);
+        }
+    });
+}
+
+function findMostFrequent(arr) {
+    if(arr.length === 0)
+        return null;
+
+    let freqMap = {};
+    let maxCount = 0;
+    let mostFrequentItem = null;
+
+    arr.forEach(function(item) {
+        freqMap[item] = (freqMap[item] + 1) || 1;
+        if (freqMap[item] > maxCount) {
+            maxCount = freqMap[item];
+            mostFrequentItem = item;
+        }
+    });
+
+    return mostFrequentItem;
 }
 
 function displayUserData(userData) {
+    // Clear old data
+    $('#selectedUserPicture').empty();
+    $('#selectedUserInfo').empty();
+    $('#selectedUserMoods').empty();
+    $('#selectedUserWebsites').empty();
 
+    if (!userData) {
+        $('#noUserSelected').append('<em><p style=\"color:gray;\">No user selected</p></em>');
+        return;
+    }
+
+    else {
+        $('#noUserSelected').empty();
+
+        var webpageList = $('<ul>');
+        userData.webpages.forEach(function (webpage) {
+            webpageList.append('<li>URL: ' + webpage.urls + '</li>');
+        });
+
+        var moodList = $('<ul>');
+        userData.moods.forEach(function (m) {
+            moodList.append('<li>Mood: ' + m.mood + '</li>');
+        });
+
+        let moods = userData.moods.map(m => m.mood);
+        const mostFrequentMood = findMostFrequent(moods);
+        let webpages = userData.webpages.map(w => w.urls);
+        let mostFrequentWebsite = findMostFrequent(webpages);
+
+        var userImage = $('<img>').attr('src', '/static/images/profile.png').css({ 'max-width': '200px', 'max-height': '200px' });
+        userImage.addClass('img-fluid');
+        $('#selectedUserPicture').append(userImage);
+        $('#selectedUserInfo').append('<p></p><p></p>');
+        $('#selectedUserInfo').append('<p><strong>Name:</strong>  ' + userData.name + '</p>');
+        $('#selectedUserInfo').append('<p><strong>Last active:</strong>  ' + userData.active + '</p>');
+        $('#selectedUserInfo').append('<p><strong>Most experienced mood:</strong>  ' + mostFrequentMood + '</p>');
+        $('#selectedUserInfo').append('<p><strong>Most visited website:</strong>  ' + mostFrequentWebsite + '</p>');
+
+        $('#selectedUserMoods').append('<p>User Moods:</p>').append(moodList);
+        $('#selectedUserWebsites').append('<p>User Visited pages:</p>').append(webpageList);
+    }
 }
